500bd3df37eeabecaf99c9655cedd315def0af2d
nik-lazarenko <nikitalazarenko@gmail.com>
2024-04-10 17:26:09 +0300

    task_solution

--- task-1/bin/surf_dart_courses_template.dart
+++ task-1/bin/surf_dart_courses_template.dart
@@ -1,4 +1,4 @@
-import 'package:surf_dart_courses_template/surf_dart_courses_template.dart' as surf_dart_courses_template;
+import 'package:surf_dart_courses_template/task_1.dart' as surf_dart_courses_template;
 
 void main(List<String> arguments) {
   print('Hello world: ${surf_dart_courses_template.calculate()}!');
--- task-1/test/surf_dart_courses_template_test.dart
+++ task-1/test/surf_dart_courses_template_test.dart
@@ -1,4 +1,4 @@
-import 'package:surf_dart_courses_template/surf_dart_courses_template.dart';
+import 'package:surf_dart_courses_template/task_1.dart';
 import 'package:test/test.dart';
 
 void main() {
