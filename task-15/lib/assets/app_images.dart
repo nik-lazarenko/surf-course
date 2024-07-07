9da140c2fb07f00ce05b7dce37518f79c7860c54
nikitalazarenko <nikitalazarenko@gmail.com>
2024-07-07 17:24:49 +0300

    task-15

---
+++ task-15/lib/assets/app_images.dart
@@ -0,0 +1,3 @@
+abstract class AppImages {
+  static const ball = 'assets/images/3117.png', bg = 'assets/images/bg.png';
+}
---
+++ task-15/lib/assets/app_strings.dart
@@ -0,0 +1,7 @@
+abstract class AppStrings {
+  static const error = 'Весьма Сомнительно!',
+      prediction = 'Можешь быть в этом уверен',
+      touchSphere = 'Нажмите на шар или потрясите телефон',
+      magicBallApi = 'https://eightballapi.com/api',
+      reading = 'Чтение данных';
+}
